7ac0f0e56731b3b029c9357d305b02511358abe3
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-08-09 09:13:37 -0700

    Change favicon from gatsby logo to josh drentlaw web design logo. Still waiting on favicon from client.

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         background_color: `#000000`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/assets/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/code.svg`, // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-styled-components',
